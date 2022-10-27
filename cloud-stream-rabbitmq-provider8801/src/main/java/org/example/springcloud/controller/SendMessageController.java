0a88a4cde798c6bbed6d599bb5d06b6037243232
zwl <1831622781@qq.com>
2022-10-27 15:46:56 +0800

    20221027 third

---
+++ cloud-stream-rabbitmq-provider8801/src/main/java/org/example/springcloud/controller/SendMessageController.java
@@ -0,0 +1,20 @@
+package org.example.springcloud.controller;
+
+import org.example.springcloud.service.IMessageProvider;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+@RestController
+public class SendMessageController
+{
+    @Resource
+    private IMessageProvider messageProvider;
+
+    @GetMapping(value = "/sendMessage")
+    public String sendMessage()
+    {
+        return messageProvider.send();
+    }
+}
---
+++ cloud-stream-rabbitmq-provider8801/src/main/java/org/example/springcloud/service/IMessageProvider.java
@@ -0,0 +1,8 @@
+package org.example.springcloud.service;
+
+public interface IMessageProvider
+{
+    public String send() ;
+}
+
+
